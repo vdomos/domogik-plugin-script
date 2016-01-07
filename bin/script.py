d2f0abdc9c8f75aefb8aa9b9190c70e98c8ffa0c
vdomos <domos.vesta@gmail.com>
2016-01-07 15:25:05 +0100

    PEP8 pass

--- bin/script.py
+++ bin/script.py
@@ -22,7 +22,7 @@ along with Domogik. If not, see U{http://www.gnu.org/licenses}.
 Plugin purpose
 ==============
 
-Run script for a command or a script for a sensor.
+Run Linux script/program for a command or a sensor.
 
 Implements
 ==========
@@ -46,117 +46,113 @@ import time
 
 
 class XplScriptManager(XplPlugin):
-	""" 
-	"""
+    """
+    """
 
-	def __init__(self):
-		""" Init plugin
-		"""
-		XplPlugin.__init__(self, name='script')
+    def __init__(self):
+        """ Init plugin
+        """
+        XplPlugin.__init__(self, name='script')
 
-		# check if the plugin is configured. If not, this will stop the plugin and log an error
-		if not self.check_configured():
-			return
-			
-		### get the devices list
-		# for this plugin, if no devices are created we won't be able to use devices.
-		self.devices = self.get_device_list(quit_if_no_device = True)
-		#print(self.devices)		# List devices créés dans plugin ?
+        # check if the plugin is configured. If not, this will stop the plugin and log an error
+        if not self.check_configured():
+            return
 
-				
-		# Init script functions
-		self.script  = Script(self.log)
-		
-		### For each device
-		for a_device in self.devices:
-			#self.log.info(u"a_device:   %s" % format(a_device))
-			
-			device_name = a_device["name"]										# Ex.: "Conso Elec Jour"
-			device_typeid = a_device["device_type_id"]							# Ex.: "script.info_number | script.info_binary | script.info_string | script.action"
-			device_statname = device_typeid.replace('.', '_')					# Ex.: "script_info_number | script_info_binary | script_info_string | script_action"
-			command_script = self.get_parameter_for_feature(a_device, "xpl_stats", "stat_" + device_statname, "program")	# Ex.: "/home/user/getElec.sh -jour"
-			if device_typeid != "script.action":								# Shedule only script_info_* scripts
-				command_interval = self.get_parameter(a_device, "interval")		# Ex.: "60" in secondes
-				self.log.info(u"==> Device '{0}' ({1}) to call = '{2}' with interval = {3}s".format(device_name, device_typeid, command_script, command_interval))
-				if command_interval > 0:
-					thr_name = "dev_{0}-{1}".format(a_device['id'], "script_info")
-					self.log.info(u"==> Launch script thread '%s' for '%s' device !" % (thr_name, device_name))
-					threads = {}
-					threads[thr_name] = threading.Thread(None, 
-										self.script.runScheduledCmd,
-										thr_name,
-										(self.log,
-											device_name,
-											device_statname,
-											command_script, 
-											command_interval,
-											self.send_xpl,
-											self.get_stop()
-										),
-									{})
-					threads[thr_name].start()
-					self.register_thread(threads[thr_name])
-					self.log.info(u"==> Wait some time before running the next scheduled script ...")
-					time.sleep(5)		# Wait some time to not start the script with the same interval et the same time.
-				else:
-					self.log.info(u"==> Script thread '%s' for '%s' device is DISABLED (interval < 0) !" % (thr_name, device_name))
-					
-			else:
-				self.log.info(u"==> Device '{0}' ({1}) to call = '{2}'".format(device_name, device_typeid, command_script))
-		
-		
-		# Create listeners
-		self.log.info(u"==> Creating listener for Script")    
-		Listener(self.scriptCmnd_cb, self.myxpl, {'xpltype': 'xpl-cmnd', 'schema': 'exec.basic'})
-				
-		self.ready()
+        # ### get the devices list
+        # for this plugin, if no devices are created we won't be able to use devices.
+        self.devices = self.get_device_list(quit_if_no_device=True)
+        # print(self.devices)        # List devices créés dans plugin ?
+
+        # Init script functions
+        self.script = Script(self.log)
+
+        # ### For each device
+        for a_device in self.devices:
+            # self.log.info(u"a_device:   %s" % format(a_device))
+
+            device_name = a_device["name"]                                      # Ex.: "Conso Elec Jour"
+            device_typeid = a_device["device_type_id"]                          # Ex.: "script.info_number | script.info_binary | script.info_string | script.action"
+            device_statname = device_typeid.replace('.', '_')                   # Ex.: "script_info_number | script_info_binary | script_info_string | script_action"
+            command_script = self.get_parameter_for_feature(a_device, "xpl_stats", "stat_" + device_statname, "program")    # Ex.: "/home/user/getElec.sh -jour"
+            if device_typeid != "script.action":                                # Shedule only script_info_* scripts
+                command_interval = self.get_parameter(a_device, "interval")     # Ex.: "60" in secondes
+                self.log.info(u"==> Device '{0}' ({1}) to call = '{2}' with interval = {3}s".format(device_name, device_typeid, command_script, command_interval))
+                if command_interval > 0:
+                    thr_name = "dev_{0}-{1}".format(a_device['id'], "script_info")
+                    self.log.info(u"==> Launch script thread '%s' for '%s' device !" % (thr_name, device_name))
+                    threads = {}
+                    threads[thr_name] = threading.Thread(None,
+                                            self.script.runScheduledCmd,
+                                            thr_name,
+                                            (self.log,
+                                                device_name,
+                                                device_statname,
+                                                command_script,
+                                                command_interval,
+                                                self.send_xpl,
+                                                self.get_stop()
+                                            ),
+                                        {})
+                    threads[thr_name].start()
+                    self.register_thread(threads[thr_name])
+                    self.log.info(u"==> Wait some time before running the next scheduled script ...")
+                    time.sleep(5)        # Wait some time to not start the script with the same interval et the same time.
+                else:
+                    self.log.info(u"==> Script thread '%s' for '%s' device is DISABLED (interval < 0) !" % (thr_name, device_name))
+
+            else:
+                self.log.info(u"==> Device '{0}' ({1}) to call = '{2}'".format(device_name, device_typeid, command_script))
+
+        # Create listeners
+        self.log.info(u"==> Creating listener for Script")
+        Listener(self.scriptCmnd_cb, self.myxpl, {'xpltype': 'xpl-cmnd', 'schema': 'exec.basic'})
+
+        self.ready()
 
 
+    def scriptCmnd_cb(self, message):
+        """ Call script lib for run program
+            @param
+            message :    xpl message
+            type :         Command type, must be set to "script.info_number | script.info_binary | script.action"
+            program :     Executable filename, including path and extension
+            status :     'start' for running program
+        """
+        self.log.debug(u"==> Call scriptCmnd_cb")
 
-	def scriptCmnd_cb(self, message):
-		""" Call script lib for run program
-			@param 
-			message :	xpl message
-			type : 		Command type, must be set to "script.info_number | script.info_binary | script.action"
-			program : 	Executable filename, including path and extension
-			status : 	'start' for running program
-		"""
-		self.log.debug(u"==> Call scriptCmnd_cb")
+        scripttype = message.data['type']
+        if (scripttype != "script_info_number") and (scripttype != "script_info_binary") and (scripttype != "script_action"):
+            self.log.error(u"### This command type %s' is not for Domogik Script plugin" % message.data['type'])
+            return
+        if message.data['status'] != "start":
+            self.log.error(u"### This command with status '%s' is not for Domogik Script plugin" % message.data['status'])
+            return
+        program = message.data['program'].strip()
 
-		scripttype = message.data['type']
-		if (scripttype != "script_info_number") and (scripttype != "script_info_binary") and (scripttype != "script_action"):
-			self.log.error(u"### This command type %s' is not for Domogik Script plugin" % message.data['type'] )
-			return
-		if message.data['status'] != "start":
-			self.log.error(u"### This command with status '%s' is not for Domogik Script plugin" % message.data['status'] )
-			return
-		program = message.data['program'].strip()
-	
-		# Execute program
-		self.log.info(u"==> Execute requested script '%s' type '%s'" % (program, scripttype))
-		
-		# call program
-		resultcmd = self.script.runCmd(program, scripttype)		# resultcmd = "executed|value|failed"
- 
-		# Send ACK xpl-trig message to xpl-cmnd command.
-		self.log.debug(u"==> Send xpl-trig msg for script '%s' with return '%s'" % (program, resultcmd))
-		self.send_xpl("xpl-trig", {"program" : program, "type" : scripttype, "status" : resultcmd})
+        # Execute program
+        self.log.info(u"==> Execute requested script '%s' type '%s'" % (program, scripttype))
+
+        # call program
+        resultcmd = self.script.runCmd(program, scripttype)        # resultcmd = "executed|value|failed"
+
+        # Send ACK xpl-trig message to xpl-cmnd command.
+        self.log.debug(u"==> Send xpl-trig msg for script '%s' with return '%s'" % (program, resultcmd))
+        self.send_xpl("xpl-trig", {"program": program, "type": scripttype, "status": resultcmd})
 
 
-	def send_xpl(self, type, data):
-		""" Send data on xPL network
-			@param data : data to send (dict)
-		"""
-		msg = XplMessage()
-		msg.set_type(type)
-		msg.set_schema("exec.basic")
-		for element in data:
-			msg.add_data({element : data[element]})
-		self.log.debug(u"==> Send xpl message...")
-		self.log.debug(msg)
-		self.myxpl.send(msg)
+    def send_xpl(self, type, data):
+        """ Send data on xPL network
+            @param data : data to send (dict)
+        """
+        msg = XplMessage()
+        msg.set_type(type)
+        msg.set_schema("exec.basic")
+        for element in data:
+            msg.add_data({element: data[element]})
+        self.log.debug(u"==> Send xpl message...")
+        self.log.debug(msg)
+        self.myxpl.send(msg)
 
 
 if __name__ == "__main__":
-	XplScriptManager()
-
+    XplScriptManager()
