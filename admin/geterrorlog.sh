4e7e3317f8dee002f269ac8561b634c0bfd94a1d
vdomos <domos.vesta@gmail.com>
2016-01-23 23:44:59 +0100

    No xPl Version + more Datatype sensors

--- admin/geterrorlog.sh
+++ admin/geterrorlog.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ERROR=$(grep ERROR /var/log/domogik/script.log |sort -rnk1,2 | head -10)
+ERROR=$(egrep 'WARNING|ERROR' /var/log/domogik/script.log |sort -rnk1,2 | head -10)
 if [ -z "$ERROR" ]
 then
 	echo "Hooray, No error."
